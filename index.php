59f3222d0ccd0d1ef0d28ab3c08c5257e8be6acc
Muhammad Usama <126102571+US4M4-K19G@users.noreply.github.com>
2023-12-21 15:45:31 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title> Header And Fotter </title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+	
+<body>
+<header>
+	<ul id="left-nav">
+	<img src="Untitfled.png" id="logo">
+	</ul>
+	<ul id="right-nav">
+	<li><a href="index.php">Home</a></li>
+	<li><a href="reg form.php">Register</a></li>
+	<li><a href="tableview.php">All Info</a></li>
+	</ul>
+	</header>
+	
+	
+	
+</body>
+<footer style="clear:both" >
+	&copy;2022 By Muhammad Usama
+	
+</footer>
+</html>
